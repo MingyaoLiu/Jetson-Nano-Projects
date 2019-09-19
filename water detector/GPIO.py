ccf20cc4c7559abe8a5404c70bed97d52bb26134
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2019-09-19 12:16:39 -0400

    Update GPIO.py

--- water detector/GPIO.py
+++ water detector/GPIO.py
@@ -3,15 +3,16 @@ import time
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+import datetime
 
 while True:
     while not GPIO.input(21):
         pass
-    print("Closed Circuit")
+    print("Closed Circuit", )
     while GPIO.input(21):
         pass
-    print("Opened Circuit")
+    now = datetime.datetime.now()
+    print("Opened Circuit", now.date)
     sleep(1)
 
 GPIO.cleanup(21)
