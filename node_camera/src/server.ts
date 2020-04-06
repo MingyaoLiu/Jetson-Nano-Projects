f98e17dc56c33c1e4e735ffb7ea1b8b214633ca5
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2020-04-06 15:42:38 -0400

    test 1 video stream

---
+++ node_camera/src/server.ts
@@ -0,0 +1,87 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Express, Request, Response, Router } from "express";
+const rateLimit = require("express-rate-limit");
+import fs from "fs";
+import http from "http";
+import https from "https";
+import path from "path";
+import { Codec, SensorMode, StillCamera, StreamCamera } from "pi-camera-connect";
+
+
+let port = 80;
+
+if (fs.existsSync("./isDev")) port = 8000;
+
+const app = express();
+// Cors
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// bodyparser
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+
+
+// enable rate limiter
+const apiLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 100,
+});
+app.use("/api/", apiLimiter);
+
+
+// Define Express routes
+app.use("/", express.static(path.join(__dirname, "www")));
+
+
+const router = Router();
+
+router.get("/api/cam", (req: Request, res: Response) => {
+    console.log("API CAM");
+    const streamCamera = new StreamCamera({
+        codec: Codec.H264,
+        width: 1280,
+        height: 720,
+    });
+
+    const videoStream = streamCamera.createStream();
+    const head = {
+        "Content-Length": 99999999,
+        "Content-Type": "video/mp4",
+        };
+    res.writeHead(200, head);
+
+    videoStream.pipe(res);
+
+    streamCamera.startCapture().then(() => {
+        console.log("stream start capture");
+        setTimeout(() => streamCamera.stopCapture(), 5000);
+    });
+});
+
+app.use(router);
+
+http.createServer({}, app).listen(port, () => {
+    console.log("SERVER", " started at https://localhost:", port);
+});
+
+
+// const stillCamera = new StillCamera({
+//     width: 1280,
+//     height: 720,
+// });
+
+// stillCamera.takeImage()
+// .then((image) => {
+//     let imagePath = path.join(__dirname, "www/still-image.jpg");
+//     fs.writeFileSync(imagePath, image);
+// });
---
+++ node_camera/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        server: path.join(__dirname, 'src/server.ts'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    target: 'node',
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    },
+    node: {
+        // Need this when working with express, otherwise the build fails
+        __dirname: false,   // if you don't put this is, __dirname
+        __filename: false,  // and __filename return blank or /
+    },
+    // externals: [nodeExternals()], // Need this to avoid error when working with Express
+    module: {
+        rules: [
+            {
+                // Transpiles ES6-8 into ES5
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "ts-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin([
+            {
+                from: "src/www",
+                to: "www",
+                toType: 'dir',
+            },
+            // {
+            //     from: 'src/tensorvortex.com-secrets/crt/*.pem',
+            //     to: 'tensorvortex.com-secrets/crt/*.pem'
+            // },
+        ]),
+    ]
+}
