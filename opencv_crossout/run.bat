56925f3a1456eccfbef2d3d460ebdc36aa1212ce
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2020-05-10 11:01:40 -0400

    Update run.bat

--- opencv_crossout/run.bat
+++ opencv_crossout/run.bat
@@ -1,18 +1,14 @@
 
-
 if exist "C:\ProgramData\Anaconda3\_conda.exe" (
     echo "Anaconda Already Installed at C:\ProgramData\Anaconda3"
 ) else (
     echo "Installing Anaconda at C:\ProgramData\Anaconda3"
-    call curl https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86_64.exe -O Anaconda3-Windows-x86_64.exe
-
-    start /wait "" ".\Anaconda3-Windows-x86_64.exe" /InstallationType=AllUsers /AddToPath=1 /RegisterPython=1 /S /D=C:\ProgramData\Anaconda3
-
-    del ".\Anaconda3-Windows-x86_64.exe"
+    curl https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86_64.exe -O
+    start /wait "" Anaconda3-2020.02-Windows-x86_64.exe /InstallationType=AllUsers /AddToPath=1 /RegisterPython=1 /S /D=C:\ProgramData\Anaconda3
+    del Anaconda3-2020.02-Windows-x86_64.exe
 )
 
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
-
 call conda env create -f environment.yml
 call conda activate crossoutML
 call python main.py
